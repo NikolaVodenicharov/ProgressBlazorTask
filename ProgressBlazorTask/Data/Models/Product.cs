0c63a3b879c00681130e501a569a13125c8799d1
nikola <nikolavodeni4arov@gmail.com>
2022-06-29 14:24:23 +0300

    Add project files.

---
+++ ProgressBlazorTask/Data/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace ProgressBlazorTask.Data.Models
+{
+    public class Product
+    {
+        public int? ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int UnitInStock { get; set; }
+
+        public DateTime DeliveryOn { get; set; }
+    }
+}
